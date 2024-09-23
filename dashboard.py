633cf1673f636d8a950457c888f6e1fdee2bb55c
mjstierman <matthew.stierman@alumni.du.edu>
2024-09-22 23:31:06 -0600

    Moved everything from dashboard to reports because that's what they became. New dashboard from scratch which emulates the spending digest from the old excel budget spreadsheets. Pivot table works; need to do categorical budget (envelope) spending.

--- dashboard.py
+++ dashboard.py
@@ -1,94 +1,43 @@
-# This is the dashboard of finances.
-# 
-# $THIS_MONTH:
-# Net MTD         (+/- $$$)
-# Digest of spending categories
-# e.g. Food (-$)
-#	   Transport (-$)
-#      etc. (-$)... 
-#
-# Accounts as of Latest Update:
-# Checking      ($)
-# Savings       ($)
-# Retirement    ($)
-# Real Estate   ($)
-# Auto Book Val ($)
-# Mortgage      (-$)
-# Student Loan  (-$)
-# Auto loan     (-$)
-# NET WORTH:    ($SUM)
-
-try:
-	import matplotlib
-except:
-	sys.exit("This script requires matplotlib. Please install 'pip3 install matplotlib'")
-	exit()
-
-try:
-	import pandas
-except:
-	sys.exit("This script requires pandas. Please install 'pip3 install pandas'")
-	exit()
+# Generates a dashboard of spending for the current month.
 
 import argparse
+import calendar
+import datetime
 import pandas
-from datetime import datetime
 
 # Grab input from cli arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--accounts", type=open, default="accounts.csv", help="Location of list of accounts in csv format. Default is accounts.csv")
 parser.add_argument("-d", "--daybook", type=open, default="daybook.csv", help="Location of the daybook/journal in csv format. Default is daybook.csv")
-parser.add_argument("-s", "--start", type=ascii, help="Format YYYY-MM-DD. Start Date to filter data for a timeframe. Default is the beginning of records.")
-parser.add_argument("-e", "--end", type=ascii, help="Format YYYY-MM-DD. End Date to filter data for a timeframe. Default is the end of records.")
 args = parser.parse_args()
 
-# Set up some variables 
-#
-# Would like to add some validation here for the start_date and end_date to ensure ISO
-# Would also like the default start/end to be the actual beginning/end of the records
-# rather than, essentially, a 10000 year span.
-#
-# todays_date is currently unused.
-todays_date = datetime.today().strftime('%Y-%m-%d')
-if args.start:
-	start_date = args.start
-else:
-	start_date = "'0001-01-01'"
-if args.end:
-	end_date = args.end
-else:
-	end_date = "'9999-12-31'"
-accounts_data = pandas.read_csv(args.accounts)
+# Input vars
 daybook_data = pandas.read_csv(args.daybook)
 
-# This Month dashboard
-# def envelope():
-	# list budget catagories,
-	# total spend in each OpEx category
-	# and how much is left in the envelope
+# Do some time math
+todays_date = datetime.date.today()
+this_year = datetime.date.today().year
+this_month = datetime.date.today().month
+last_day_calc = calendar.monthrange(this_year, this_month)
+last_day = last_day_calc[1]
+start_date = todays_date.replace(day=1)
+end_date = todays_date.replace(day=last_day)
+
+def budget_spend():
+	print("This is where envelope spending goes.")
 
 def spending_report():
 	# Format the 'Date' column to be a date datatype
 	daybook_data['Date'] = pandas.to_datetime(daybook_data['Date'], format='%Y-%m-%d')
-	# Filter the DataFrame based on whether start and end dates were provided
-	filtered_df = daybook_data.loc[(daybook_data['Date'] >= start_date) & (daybook_data['Date'] <= end_date)]
-	table = filtered_df.pivot_table(index="Date", columns="Category", values="Amount", aggfunc='sum', fill_value=0, margins=True)
+	converted_start_date = pandas.to_datetime(start_date, format='%Y-%m-%d')
+	converted_end_date = pandas.to_datetime(end_date, format='%Y-%m-%d')
+	# Filter the DataFrame based on first and last days of the month
+	filtered_df_1 = daybook_data.loc[(daybook_data['Date'] >= converted_start_date)]
+	filtered_df_2 = filtered_df_1.loc[(daybook_data['Date'] <= converted_end_date)]
+	# spit it into a pivot table
+	table = filtered_df_2.pivot_table(index="Date", columns="Category", values="Amount", aggfunc='sum', fill_value=0, margins=True)
 	# Print the resulting pivot table
 	print(table)
 
-# Net Worth Calculator
-def net_worth():
-	# Format the 'Date' column to be a date datatype
-	accounts_data['Date'] = pandas.to_datetime(accounts_data['Date'], format="%Y-%m-%d")
-	# Filter the DataFrame based on whether start and end dates were provided
-	filtered_df = accounts_data.loc[(accounts_data['Date'] >= start_date) & (accounts_data['Date'] <= end_date)]
-	# define pivot table bounds
-	table = filtered_df.pivot_table(index="Date", columns="Class", values="Balance", aggfunc='sum', fill_value=0, margins=True)
-	# Print the resulting pivot table
-	print(table)
-
-print("################ Report for",start_date,"through",end_date,"#################")
-print("################################ Spending Report ##############################")
+budget_spend()
+print()
 spending_report()
-print("################################### Net Worth #################################")
-net_worth()
---
+++ reports.py
@@ -0,0 +1,63 @@
+# Generates financial reports over a given timeframe
+
+try:
+	import pandas
+except:
+	sys.exit("This script requires pandas. Please install 'pip3 install pandas'")
+	exit()
+
+import argparse
+import pandas
+from datetime import datetime
+
+# Grab input from cli arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--accounts", type=open, default="accounts.csv", help="Location of list of accounts in csv format. Default is accounts.csv")
+parser.add_argument("-d", "--daybook", type=open, default="daybook.csv", help="Location of the daybook/journal in csv format. Default is daybook.csv")
+parser.add_argument("-s", "--start", type=ascii, help="Format YYYY-MM-DD. Start Date to filter data for a timeframe. Default is the beginning of records.")
+parser.add_argument("-e", "--end", type=ascii, help="Format YYYY-MM-DD. End Date to filter data for a timeframe. Default is the end of records.")
+args = parser.parse_args()
+
+# Set up some variables 
+#
+# Would like to add some validation here for the start_date and end_date to ensure ISO
+# Would also like the default start/end to be the actual beginning/end of the records
+# rather than, essentially, a 10000 year span.
+
+if args.start:
+	start_date = args.start
+else:
+	start_date = "'0001-01-01'"
+if args.end:
+	end_date = args.end
+else:
+	end_date = "'9999-12-31'"
+accounts_data = pandas.read_csv(args.accounts)
+daybook_data = pandas.read_csv(args.daybook)
+
+def spending_report():
+	# Format the 'Date' column to be a date datatype
+	daybook_data['Date'] = pandas.to_datetime(daybook_data['Date'], format='%Y-%m-%d')
+	# Filter the DataFrame based on whether start and end dates were provided
+	filtered_df = daybook_data.loc[(daybook_data['Date'] >= start_date) & (daybook_data['Date'] <= end_date)]
+	table = filtered_df.pivot_table(index="Date", columns="Category", values="Amount", aggfunc='sum', fill_value=0, margins=True)
+	# Print the resulting pivot table
+	print(table)
+
+# Net Worth Calculator
+def net_worth():
+	# Format the 'Date' column to be a date datatype
+	accounts_data['Date'] = pandas.to_datetime(accounts_data['Date'], format="%Y-%m-%d")
+	# Filter the DataFrame based on whether start and end dates were provided
+	filtered_df = accounts_data.loc[(accounts_data['Date'] >= start_date) & (accounts_data['Date'] <= end_date)]
+	# define pivot table bounds
+	table = filtered_df.pivot_table(index="Date", columns="Class", values="Balance", aggfunc='sum', fill_value=0, margins=True)
+	# Print the resulting pivot table
+	print(table)
+
+print("################ Report for",start_date,"through",end_date,"#################")
+print("############################### Total Spend Report ############################")
+spending_report()
+print("################################### Net Worth #################################")
+net_worth()
+print("################################## End Reports ################################")
